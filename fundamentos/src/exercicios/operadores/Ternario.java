becd5348462111d73b93b6c6fafc01727512a395
alfxavier <alfxavier@gmail.com>
2023-06-28 11:12:08 -0300

    até aula 54.

---
+++ fundamentos/src/exercicios/operadores/Ternario.java
@@ -0,0 +1,22 @@
+package exercicios.operadores;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+		double media = 6.0;
+		String resultadoFinal = media >= 7.0 ? "aprovado." : "em recuperação.";
+		
+		System.out.println("O aluno está "+ resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = true;
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim." : "Não.";
+		
+		System.out.printf("Tem desconto? %s", resultado);
+		
+
+	}
+
+}
