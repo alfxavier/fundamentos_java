7f8a14221143ae365a3b8b6d397af7cb5a91ecdd
alfxavier <alfxavier@gmail.com>
2023-06-28 07:16:40 -0300

    primeiro comite

---
+++ fundamentos/src/exercicios/Aritmeticos.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class Aritmeticos {
+
+	public static void main(String[] args) {
+		
+		System.out.println(2 + 3);
+		
+		var x = 34.45;
+		double y = 2.2;
+		
+		System.out.println(x + y);
+
+	}
+
+}
